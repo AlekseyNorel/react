f17fb750243fde2d10c13bcfc3eb16242adc6f0d
AlekseyNorel <aleksei.julivi@gmail.com>
2023-08-16 21:35:41 +0300

    init

---
+++ react_2023/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {UserComponent} from "./components/UserComponent/UserComponent";
+
+function App() {
+  return (
+    <>
+      <UserComponent />
+    </>
+  );
+}
+
+export default App;
